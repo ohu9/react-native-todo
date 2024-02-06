96475b024f2e01983377e39018b10f9847affb76
ohu9 <oliviah221@gmail.com>
2024-02-06 16:10:47 -0500

    implemented useState hooks

--- App.js
+++ App.js
@@ -1,36 +1,54 @@
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, TextInput, Alert, Flatlist, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, Alert, FlatList, SafeAreaView } from 'react-native';
 
 const DATA = [
   {
     id: '1',
-    title: 'Meditation'
+    title: 'Meditation',
+    completed: false,
   },
   {
     id: '2',
-    title: 'Coding'
+    title: 'Coding',
+    completed: false,
   },
   {
     id: '3',
-    title: 'Journaling'
+    title: 'Journaling',
+    completed: false,
   }
 ]
 
-const TodoTItem = (props) => (
+const TodoItem = (props) => (
   <View style={styles.item}>
-    <Text style={style.itemText}>{props.item.title}</Text>
+    <Text style={styles.itemText}>{props.item.title}</Text>
   </View>
 )
 
 export default function App() {
+  const [items, setItems] = useState(DATA);
+  const [text, setText] = useState("");
+
+  const addNewTodo = () => {
+    let newTodo = {
+      id: items.length + 1,
+      title: text,
+      completed: false,
+    };
+
+    setItems([...items, newTodo]);
+    setText("");
+  }
   return (
-    <SafeAreaView style={styles.container}>
+    <SafeAreaView  style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
-      <TextInput style={styles.input}/>
-      <Button title='Start' onPress={() => Alert.alert('Simple Button pressed')} />
-      <Flatlist
-        data={DATA}
+      <TextInput style={styles.input} onChangeText={setText} value ={text}/>
+      <Button title='Add Todo' onPress={addNewTodo} />
+      <FlatList
+        style={styles.list}
+        data={items}
         renderItem={({item}) => <TodoItem item={item} />}
         keyExtractor={item => item.id}
         />
@@ -50,15 +68,21 @@ const styles = StyleSheet.create({
     width: 200,
     borderWidth: 1,
     padding: 10,
+    margin: 10,
     borderColor: 'gray',
   },
+  list: {
+    alignSelf: 'stretch',
+    alignContent: 'center',
+  },
   item: {
-    backgroundColor: 'light-blue',
-    padding: 2,
+    backgroundColor: '#adc7b4',
+    padding: 17,
     marginVertical: 8,
-    marginHorizontal: 16
+    marginHorizontal: 10,
+    borderRadius: 10,
   },
   itemText: {
-    color: '#ffff'
+    color: '#fff'
   }
 });
