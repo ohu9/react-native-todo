359899275d1ef7feda019748a7dc372fd0398e2e
ohu9 <oliviah221@gmail.com>
2024-02-06 15:25:37 -0500

    framework of to-do app

--- App.js
+++ App.js
@@ -1,12 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, Alert, Flatlist, SafeAreaView } from 'react-native';
+
+const DATA = [
+  {
+    id: '1',
+    title: 'Meditation'
+  },
+  {
+    id: '2',
+    title: 'Coding'
+  },
+  {
+    id: '3',
+    title: 'Journaling'
+  }
+]
+
+const TodoTItem = (props) => (
+  <View style={styles.item}>
+    <Text style={style.itemText}>{props.item.title}</Text>
+  </View>
+)
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
-    </View>
+      <TextInput style={styles.input}/>
+      <Button title='Start' onPress={() => Alert.alert('Simple Button pressed')} />
+      <Flatlist
+        data={DATA}
+        renderItem={({item}) => <TodoItem item={item} />}
+        keyExtractor={item => item.id}
+        />
+    </SafeAreaView>
   );
 }
 
@@ -17,4 +45,20 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    height: 40,
+    width: 200,
+    borderWidth: 1,
+    padding: 10,
+    borderColor: 'gray',
+  },
+  item: {
+    backgroundColor: 'light-blue',
+    padding: 2,
+    marginVertical: 8,
+    marginHorizontal: 16
+  },
+  itemText: {
+    color: '#ffff'
+  }
 });
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # react-native-todo
 Basic to-do app created for iOS using React Native
+
+Based on tutorial by webdecoded: https://www.youtube.com/watch?v=Zawba2eABsU
